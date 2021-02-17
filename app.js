54e57100d5563bb3e0cfc47691d612ecc9f44747
Marlom <falconsoft.3d@gmail.com>
2021-02-17 01:35:03 +0100

    sc

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT;
+const Server = require('./models/server');
+const server = new Server();
+
+server.listen();
  
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+
  
-app.listen(port, () =>{
-    console.clear();
-    console.log(`Servidor corriendo por: ${port}`)
-})
---
+++ models/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Middleware
+        this.middleware();
+
+        // Rutas de mi aplicacion
+        this.route();
+    }
+
+    middleware(){
+        // Directorio Publico
+        this.app.use( express.static('public') )
+    }
+
+    route(){
+        this.app.get('/api', (req, res) => {
+            res.json({
+                ok:true,
+                msg: 'get API'
+            });
+          });
+          
+          this.app.put('/api', (req, res) => {
+            res.json({
+                msg: 'put API'
+            });
+          });
+          
+          this.app.post('/api', (req, res) => {
+            res.json({
+                msg: 'post API'
+            });
+          });
+          
+          this.app.delete('/api', (req, res) => {
+            res.json({
+                msg: 'delete API'
+            });
+          });
+
+          this.app.patch('/api', (req, res) => {
+            res.json({
+                msg: 'patch API'
+            });
+          });
+            
+    }
+    listen(){
+        this.app.listen(this.port , () =>{
+            console.clear();
+            console.log(`Servidor corriendo por: ${this.port}`);
+        })
+    }
+}
+
+
+module.exports = Server;
